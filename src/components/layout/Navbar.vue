93ac103e50aa184f07ca9ddb0b9605351363d0f3
Max <maxweru@gmail.com>
2021-05-08 00:33:29 -0400

    Agregade algunos elementos de le barra de tareas

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-      A so pota madre
+  <div class="navbar-contenedor">
+    
+     <div class="logo">
+           <h1>Bohify</h1>
+      </div>
+    
+    <nav>
+         <a href="#">Premium</a>
+         <a href="#">Support</a>
+         <a href="#">Download</a>
+         <a href="#">Sing up</a>
+         <a href="#">Log in</a>
+    </nav>
   </div>
 </template>
 
@@ -11,6 +22,29 @@ export default Vue.extend({
 })
 </script>
 
-<style>
+<style lang="scss">
+.navbar-contenedor{
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    text-decoration: none;
+    font-size: 1.6rem;
+    height: 100px;
+    gap: 30px;
+
+    .logo{
+
+        h1{
+            color: #fff;
+            margin: auto;
+            font-size: 2rem;
+        }
+    }
+    a{
+        text-decoration: none;
+        color: #fff;
+    }
+}
 
 </style>
