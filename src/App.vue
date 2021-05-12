2b959c9a250dd77d3d93b33cd68e36adf0efca1b
Max <maxweru@gmail.com>
2021-05-11 20:41:43 -0400

    ola k ase

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,19 @@
       <Navbar />
     </div>
     <router-view />
+    <Footer/>
   </div>
+
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Navbar from "@/components/layout/Navbar.vue";
+import Footer from "@/components/layout/footer.vue";
 export default Vue.extend({
   components: {
     Navbar,
+    Footer,
   },
 });
 </script>
---
+++ src/components/layout/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+alskdhjgfasjhdgfkahjsgdf
+  </div>
+</template>
+
+<script lang="js">
+export default {
+    name: "footer"
+}
+</script>
+
+<style lang="scss">
+
+</style>
