9c17a33107f784f82a7868690ae56fffe4146715
Heger Arias Santos <hegel.0218@gmail.com>
2021-05-13 19:45:10 -0400

    Todas las rutas principales agregadas, y sus componentes creados

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +7,35 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
     path: '/about',
     name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+  ,
+  {
+    path: '/playlist/:id',
+    name: 'playlist',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ListOfPlaylist.vue')
+  }
+  ,
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import(/* webpackChunkName: "about" */ '../views/serach.vue')
+  }
+  ,
+  {
+    path: '/playlists',
+    name: 'PlaylistPage',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Playlists.vue')
   }
+
 ]
 
 const router = new VueRouter({
