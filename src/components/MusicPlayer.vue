af3ce3061ac806ffd55f86cee149f6bcbe7ac142
David <raymondinf23@gmail.com>
2021-05-13 20:05:22 -0400

    Add basic structure of Vue component

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    export default Vue.extend({
+        data(){
+            return {
+                name: "David",
+            }
+        }
+    })
+</script>
