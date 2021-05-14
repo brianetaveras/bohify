c37e3f12b782f3036a844e4ea0314b8948e3c49c
David <raymondinf23@gmail.com>
2021-05-13 20:33:41 -0400

    Add player route

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/player',
+    name: 'Player',
+    component: () => import(/* webpackChunkName: "player" */ '../views/Player.vue')
   }
 ]
 
