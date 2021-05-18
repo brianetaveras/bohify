0ae014e78f078447ece078bb54486128fc9dc32a
Osiris-Matos <osirismatos29@gmail.com>
2021-05-18 17:27:43 -0400

    Agrega el componente banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+     <div class="Banner">
+        <img class="banner-image" src="../assets/banner.png" alt="Banner">
+    </div>
+
+</template>
+
+<style>
+    html{
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+
+body{
+    height: 100%;
+}
+.Banner {
+    width: 100%;
+}
+.banner-image{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+}
+
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Banner></Banner> 
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import Banner from '@/components/Banner.vue';
 export default Vue.extend({
   name: 'Home',
   components: {
     HelloWorld,
+    Banner,
   },
 });
 </script>
